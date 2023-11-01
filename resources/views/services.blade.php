132c01eed9fc25560495f7aba402554f2bde65c9
kishart <amoralyse21@gmail.com>
2023-11-01 21:02:15 +0800

    medyo okie

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -1,24 +1,47 @@
 
 @extends('layouts.navbar')
 @section('content')
-<div class="container" style="background-color:pink;">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-                <h1>this is user homepage</h1>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<style>
+    .container{
+        display: flex;
+    }
+    .card{
+        margin: 10px;
+    }
+</style>
+<div class="container">
 
-                    {{ __('You are service in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+
+<div class="card" style="width: 18rem;">
+  <img src="https://scontent.fozc1-1.fna.fbcdn.net/v/t39.30808-6/354602444_695747049230736_5534527285588015698_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=DOkToZd33fIAX8CeDlg&_nc_ht=scontent.fozc1-1.fna&oh=00_AfATM0iVMx8BehvQvsXpsfrKgj3q2d2Jgk7paxQTC7eLUQ&oe=6547C435" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-dark">Go somewhere</a>
+  </div>
 </div>
+
+<div class="card" style="width: 18rem;">
+  <img src="https://scontent.fozc1-1.fna.fbcdn.net/v/t39.30808-6/354602444_695747049230736_5534527285588015698_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=DOkToZd33fIAX8CeDlg&_nc_ht=scontent.fozc1-1.fna&oh=00_AfATM0iVMx8BehvQvsXpsfrKgj3q2d2Jgk7paxQTC7eLUQ&oe=6547C435" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-dark">Go somewhere</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img src="https://scontent.fozc1-1.fna.fbcdn.net/v/t39.30808-6/354602444_695747049230736_5534527285588015698_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=DOkToZd33fIAX8CeDlg&_nc_ht=scontent.fozc1-1.fna&oh=00_AfATM0iVMx8BehvQvsXpsfrKgj3q2d2Jgk7paxQTC7eLUQ&oe=6547C435" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-dark">Go somewhere</a>
+  </div>
+</div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
 @endsection
